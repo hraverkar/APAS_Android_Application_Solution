cb2574029f3ddeb577ac23aa122a72999eac95b3
Harshal Raverkar <harshal.raverkar@gmail.com>
2019-12-17 16:11:20 +0530

    Create basic thread example
    Example of basic Thread.

---
+++ Today/BasicThread.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Threading;
+
+namespace ConsoleApp7
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      ThreadStart tdstard = new ThreadStart(threadJob);
+      Thread thread = new Thread(tdstard);
+      thread.Start();
+      for (int i = 0; i <= 8; i++)
+        Console.WriteLine("My main therad " + i);
+      Thread.Sleep(10000);
+    }
+
+    private static void threadJob()
+    {
+      for (int i = 0; i <= 6; i++)
+        Console.WriteLine("This is Thead : : " + i);
+
+      Thread.Sleep(500);
+      
+    }
+  }
+}
